0b47a924457f7f024199a0539cfc533210f40fd8
Silvia Traversa <s.traversa98@gmail.com>
2020-06-12 18:00:45 +0200

    record linkage

--- source/recordlinkage/record-linkage.py
+++ source/recordlinkage/record-linkage.py
@@ -1,26 +1,38 @@
 import recordlinkage
 import pandas as pd
+from recordlinkage.index import Block
+from recordlinkage.preprocessing import clean
 
 
-data1 = pd.read_csv("C:/Users/strav/git/Data_quality_experimentation/data/restaurants/grouped_by_restaurant/ActiveDiner.txt", sep = "\t")
-data1.columns = ["Fonte", "Ristorante", "Indirizzo"]
+data1 = pd.read_csv("data/restaurants/grouped_by_restaurant/ActiveDiner.txt", 
+                        encoding = "UTF-8",
+                        sep = "\t",
+                        names = ["Fonte", "Ristorante", "Indirizzo"] )
 df1 = pd.DataFrame(data1)
 
-data2 = pd.read_csv("C:/Users/strav/git/Data_quality_experimentation/data/restaurants/grouped_by_restaurant/DiningGuide.txt", sep = "\t")
-data2.columns = ["Fonte", "Ristorante", "Indirizzo"]
+data2 = pd.read_csv("data/restaurants/grouped_by_restaurant/DiningGuide.txt", 
+                        encoding = "UTF-8",
+                        sep = "\t",
+                        names = ["Fonte", "Ristorante", "Indirizzo"])
 df2 = pd.DataFrame(data2)
 
-#https://recordlinkage.readthedocs.io/en/latest/notebooks/link_two_dataframes.html
-indexer = recordlinkage.Index()
-indexer.full()
-pairs = indexer.index(df1, df2)
-# print (len(pairs)) 5026779
 
+
+#recordlinkage.preprocessing.clean(s)
+"""
+#https://recordlinkage.readthedocs.io/en/latest/notebooks/link_two_dataframes.html
+
+indexer = recordlinkage.SortedNeighbourhoodIndex(on = ["Ristorante"])
+candidate_links = indexer.index(df1, df2)
+print(len(candidate_links))
 
 compare_cl = recordlinkage.Compare()
 
-compare_cl.string('Ristorante', 'Ristorante', method='jarowinkler', threshold=0.85, label='surname')
-compare_cl.string('Indirizzo', 'Indirizzo', threshold=0.85, label='address_1')
+compare_cl.string('Ristorante', 'Ristorante', threshold=0.85, label='Ristorante')
+compare_cl.string('Indirizzo', 'Indirizzo', threshold=0.85, label='Indirizzo')
 
-features = compare_cl.compute(pairs, df1, df2)
-print(features)
+features = compare_cl.compute(candidate_links, df1, df2)
+# Classification step 
+matches = features[features.sum(axis=1) > 3] 
+print(len(matches))
+"""
