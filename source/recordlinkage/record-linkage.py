a3ad794e40d7ed64b6fa39e40fdeb7b021159c4a
Silvia Traversa <s.traversa98@gmail.com>
2020-06-24 16:09:03 +0200

    record linkage toolkit

--- source/recordlinkage/record-linkage.py
+++ source/recordlinkage/record-linkage.py
@@ -4,52 +4,55 @@ import pdb
 from recordlinkage.index import Block
 from recordlinkage.preprocessing import clean
 
+# Load data
 
-data1 = pd.read_csv("data/restaurants/gbr_splitted/ActiveDiner.csv", 
-                        sep = ";") # 1 2 3_x
-                        
-df1 = pd.DataFrame(data1)
-df1.columns = ['Restaurant_1', 'Address_1', 'Country_1']
+df1 = pd.read_csv("data/restaurants/gbr_splitted/ActiveDiner.csv", sep = ";") 
+df1 = df1.add_prefix('1_')
 
-data2 = pd.read_csv("data/restaurants/gbr_splitted/DiningGuide.csv",
-                        sep = ";")
-                        
-df2 = pd.DataFrame(data2)
-df2.columns = ['Restaurant_2', 'Address_2']
-
-print(df1,df2)
+df2 = pd.read_csv("data/restaurants/gbr_splitted/DiningGuide.csv", sep = ";")
+df2 = df2.add_prefix('2_')
 
 
 # https://recordlinkage.readthedocs.io/en/latest/notebooks/link_two_dataframes.html
 
 indexer = recordlinkage.Index()
-indexer.sortedneighbourhood(left_on='Restaurant_1', right_on='Restaurant_2')
+indexer.sortedneighbourhood(left_on='1_restaurant', right_on='2_restaurant')
 candidate_links = indexer.index(df1,df2)
-print(candidate_links)
-
+for couple in candidate_links:
+        ind = couple[0]
+        print(df1.take(ind))
 
 comp = recordlinkage.Compare()
 
-comp.string('Restaurant_1', 'Restaurant_2', label='Ristorante')
-comp.string('Address_1', 'Address_2', label='Indirizzo')
+comp.string('1_restaurant', '2_restaurant', label='ristorante')
+comp.string('1_address', '2_address', label='indirizzo')
 features = comp.compute(candidate_links, df1, df2)
-print(features)
-print(features.sum(axis=1).value_counts().sort_index(ascending=False))
+# print(features)
+# print(features.sum(axis=1).value_counts().sort_index(ascending=False))
 
 # https://pbpython.com/record-linking.html
 
 potential_matches = features[features.sum(axis=1) > 1].reset_index()
-potential_matches['Score'] = potential_matches.loc[:, 'Ristorante':'Indirizzo'].sum(axis=1)
+potential_matches['Score'] = potential_matches.loc[:, 'ristorante':'indirizzo'].sum(axis=1)
 print(potential_matches)
 
-df1['ActiveDiner_Lookup'] = df1[['Restaurant_1', 'Address_1', 'Country_1']].apply(lambda x: '_'.join(x), axis=1)
 
-df2['DiningGuide_Lookup'] = df2[['Restaurant_2', 'Address_2']].apply(lambda x: '_'.join(x), axis=1)
+df1['ActiveDiner_Lookup'] = df1[['1_restaurant', '1_address', '1_country']].apply(lambda x: '_'.join(str(x)), axis=1)
+
+df2['DiningGuide_Lookup'] = df2[['2_restaurant', '2_address']].apply(lambda x: '_'.join(str(x)), axis=1)
 
 df1_lookup = df1[['ActiveDiner_Lookup']].reset_index()
 df2_lookup = df2[['DiningGuide_Lookup']].reset_index()
 
-data_merge = potential_matches.merge(df1_lookup, how='left')
+"""
+print("**********************")
+print(potential_matches)
+print(df1_lookup)
+print(df2_lookup)
+
+data_merge = potential_matches.merge(df1_lookup, how = 'left')
+
+
 final_merge = data_merge.merge(df2_lookup, how='left')
 
 cols = ['ActiveDiner', 'DiningGuide', 'Score',
@@ -57,4 +60,4 @@ cols = ['ActiveDiner', 'DiningGuide', 'Score',
 final_merge[cols].sort_values(by=['ActiveDiner', 'Score'], ascending=False)
 
 print(final_merge)
-
+"""
