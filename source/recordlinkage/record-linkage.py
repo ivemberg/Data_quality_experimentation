41a7a077daa70b8b9902a4d459cbae5e4a5f249d
Umberto <okamirvs@gmail.com>
2020-06-24 21:58:30 +0200

    merge and output csv in recorlinkage

--- source/recordlinkage/record-linkage.py
+++ source/recordlinkage/record-linkage.py
@@ -6,21 +6,22 @@ from recordlinkage.preprocessing import clean
 
 # Load data
 
-df1 = pd.read_csv("data/restaurants/gbr_splitted/ActiveDiner.csv", sep = ";") 
+df1 = pd.read_csv("../../data/restaurants/gbr_splitted/ActiveDiner.csv", sep = ";") 
 df1 = df1.add_prefix('1_')
 
-df2 = pd.read_csv("data/restaurants/gbr_splitted/DiningGuide.csv", sep = ";")
+df2 = pd.read_csv("../../data/restaurants/gbr_splitted/DiningGuide.csv", sep = ";")
 df2 = df2.add_prefix('2_')
 
 
 # https://recordlinkage.readthedocs.io/en/latest/notebooks/link_two_dataframes.html
 
 indexer = recordlinkage.Index()
+# This method is very useful when there are many misspellings in the string were used for indexing
 indexer.sortedneighbourhood(left_on='1_restaurant', right_on='2_restaurant')
 candidate_links = indexer.index(df1,df2)
 for couple in candidate_links:
         ind = couple[0]
-        print(df1.take(ind))
+        #print(df1.take(ind))
 
 comp = recordlinkage.Compare()
 
@@ -37,12 +38,17 @@ potential_matches['Score'] = potential_matches.loc[:, 'ristorante':'indirizzo'].
 print(potential_matches)
 
 
-df1['ActiveDiner_Lookup'] = df1[['1_restaurant', '1_address', '1_country']].apply(lambda x: '_'.join(str(x)), axis=1)
+df1['ActiveDiner_Lookup'] = df1[['1_restaurant', '1_address', '1_country']].apply(lambda x: ''.join(str(x)), axis=1)
 
-df2['DiningGuide_Lookup'] = df2[['2_restaurant', '2_address']].apply(lambda x: '_'.join(str(x)), axis=1)
+df2['DiningGuide_Lookup'] = df2[['2_restaurant', '2_address']].apply(lambda x: ''.join(str(x)), axis=1)
 
 df1_lookup = df1[['ActiveDiner_Lookup']].reset_index()
 df2_lookup = df2[['DiningGuide_Lookup']].reset_index()
+
+account_merge = potential_matches.merge(df1_lookup, left_on="level_0", right_index=True, how='left')
+final_merge = account_merge.merge(df2_lookup, left_on="level_1", right_index=True, how='left')
+print(final_merge)
+final_merge.to_csv('test_OKAMI.csv', header=True, sep=";", decimal=',', float_format='%.3f')
 
 """
 print("**********************")
