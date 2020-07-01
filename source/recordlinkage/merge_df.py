51f38849a1303188d9fe53c3dafe67b2d6a9b2b3
Silvia Traversa <s.traversa98@gmail.com>
2020-07-01 11:28:36 +0200

    dataframe deduplication

--- source/recordlinkage/merge_df.py
+++ source/recordlinkage/merge_df.py
@@ -31,7 +31,8 @@ for i in range(lenght):
 
 df.index = new_index
 
-# deduplication
+# DEDUPLICATION
+
 indexer = recordlinkage.Index()
 indexer.sortedneighbourhood(left_on=['restaurant'])
 candidate_links = indexer.index(df)
@@ -41,21 +42,57 @@ compare_cl.string('restaurant', 'restaurant', threshold=0.85, label='ristorante'
 compare_cl.string('neighborhood', 'neighborhood', threshold=0.85, label='quartiere')
 
 features = compare_cl.compute(candidate_links, df)
-print(features)
+
 
 matches = features[features.sum(axis=1) > 1]
-print(len(matches))
 
 potential_matches = features[features.sum(axis=1) > 1].reset_index()
 potential_matches['Score'] = potential_matches.loc[:, ['ristorante','quartiere']].sum(axis=1)
 
+# MATCHES 
 account_merge = potential_matches.merge(df, left_on="level_0", right_index=True) #, how='outer'
 final_merge = account_merge.merge(df, left_on="level_1", right_index=True)
 final_merge.dropna(axis='columns', inplace=True, how = 'all') #rimuovo solo colonne tutte NaN
 final_merge.drop(columns = ['ristorante', 'quartiere', 'Score'], inplace=True) # so che sono 1 1 2
 
+final_list = []
+all_indexes = []
 for i, row in final_merge.iterrows():
-    print(row)
+    all_indexes.append(row.level_0)
+    all_indexes.append(row.level_1)
 
+    if row.restaurant_x.lower() == row.restaurant_y.lower():
+        restaurant = row.restaurant_x
+    else:
+        restaurant = row.restaurant_x + ";" + row.restaurant_y
+    
+    if row.neighborhood_x.lower() == row.neighborhood_y.lower():
+        neighborhood = row.neighborhood_x
+    else:
+        neighborhood = row.neighborhood_x + ";" + row.neighborhood_y
+    
+    address = row.address_y
+    country = row.country_y
+    type_r = row.type_y
+    cost = row.cost_y
+    country_code = row.country_code_y
+    new_row = (restaurant, neighborhood, address, country, country_code, type_r, cost)
+    final_list.append(new_row)
+
+clean_merge = pd.DataFrame(final_list)
+clean_merge.columns = ["restaurant", "neighborhood", "address", "country", "country_code", "type_r", "cost"]
+
+df.drop(index = all_indexes, inplace = True, axis = 0)
+
+df = df.append(clean_merge)
+lenght = df.shape[0]
+new_index = []
+
+for i in range(lenght):
+    new_index.append(i)
+
+df.index = new_index
+
+df.to_csv("prova_deduplication.csv", header=True, sep=";", decimal=',', float_format='%.3f', index=False)
 
 #final_merge.to_csv("prova_deduplication.csv", header=True, sep=";", decimal=',', float_format='%.3f', index=False)
