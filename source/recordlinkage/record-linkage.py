9bee4b6a0986779499be621e89386fb31cf3c7b8
Silvia Traversa <s.traversa98@gmail.com>
2020-07-03 15:49:05 +0200

    classification method choice

--- source/recordlinkage/record-linkage.py
+++ source/recordlinkage/record-linkage.py
@@ -8,7 +8,7 @@ import merge_df
 # https://pbpython.com/record-linking.html
 
 
-def linkDB(df1, df2, type, showInfo) :
+def linkDB(df1, df2, type, classifier) :
 
 	# 1 - INDEXING
 
@@ -24,41 +24,49 @@ def linkDB(df1, df2, type, showInfo) :
 	candidate_links = indexer.index(df1,df2)
 	
 
-	# 2 COMPARISON 
+	# 2 - COMPARISON 
 	comp = recordlinkage.Compare()
-	comp.string('0_restaurant', '1_restaurant', threshold=0.5, label='ristorante')
-	comp.string('0_address', '1_address', threshold=0.5, label='indirizzoOriginale')
-	comp.string('0_neighborhood', '1_neighborhood', threshold=0.5, label='quartiere')
+	comp.string('0_restaurant', '1_restaurant', threshold=0.85, label='ristorante')
+	comp.string('0_address', '1_address', threshold=0.85, label='indirizzoOriginale')
+	comp.string('0_neighborhood', '1_neighborhood', threshold=0.85, label='quartiere')
 	
 	features = comp.compute(candidate_links, df1, df2)
 
-	# CLASSIFICATION
+	# 3 - CLASSIFICATION
 	# https://recordlinkage.readthedocs.io/en/latest/ref-classifiers.html#unsupervised
 
 	# ECM Classifier
 	ecm = recordlinkage.ECMClassifier()
 	ecm.fit_predict(features, match_index=None) # Train the classifier
 	e_matches = ecm.predict(features)
-	prob_matches = ecm.prob(features)
-	matches = []
-	for i, j in e_matches:
-		record_1 = df1.loc[i]
-		record_2 = df2.loc[j]
-		record = tuple(record_1) + tuple(record_2)
-		matches.append(record)
-	head = tuple(df1.head()) + tuple(df2.head())
-	
-	result = pd.DataFrame(matches)
-	result.columns = head
-	result.to_csv('./data/restaurants_integrated/output_recordlinkage/final_matches.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
+	print(e_matches)
+	# prob_matches = ecm.prob(features)
 
-	"""
 	# K MEANS Classifier
 	kmeans = recordlinkage.KMeansClassifier()
 	kmeans.fit_predict(features)
 	k_matches = kmeans.predict(features)
 	print(k_matches)
-	"""
+
+	matches = []
+	
+	if classifier == "ecm":
+		for i, j in e_matches:
+			record_1 = df1.loc[i]
+			record_2 = df2.loc[j]
+			record = tuple(record_1) + tuple(record_2)
+			matches.append(record)
+	elif classifier == "kmeans":
+		for i, j in k_matches:
+			record_1 = df1.loc[i]
+			record_2 = df2.loc[j]
+			record = tuple(record_1) + tuple(record_2)
+			matches.append(record)
+	
+	head = tuple(df1.head()) + tuple(df2.head())
+	result = pd.DataFrame(matches)
+	result.columns = head
+
 	return result
 
 def main():
@@ -71,9 +79,12 @@ def main():
 	df2 = df2.add_prefix('1_')
 	 #1_restaurant 1_address 1_country 1_neighbourhood 1_phone 1_type_r
 	
-	final_merge = linkDB(df1, df2, type="sortedneighbourhood", showInfo=True)
-	# final_merge.to_csv('./data/restaurants_integrated/output_recordlinkage/final_output.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
+	ecm_result = linkDB(df1, df2, type="sortedneighbourhood", classifier = "ecm")
+	km_result = linkDB(df1, df2, type="sortedneighbourhood", classifier = "kmeans")
+
 	
+	ecm_result.to_csv('./data/restaurants_integrated/output_recordlinkage/final_matches_ecm.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
+	km_result.to_csv('./data/restaurants_integrated/output_recordlinkage/final_matches_km.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
 	
 	
 if __name__ == "__main__":
