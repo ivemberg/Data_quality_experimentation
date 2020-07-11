9d2840ee58c8cbca64b67881aca1fc188dcde724
Silvia Traversa <s.traversa98@gmail.com>
2020-07-11 16:13:32 +0200

    updated with google address

--- source/recordlinkage/merge_df.py
+++ source/recordlinkage/merge_df.py
@@ -3,19 +3,19 @@ import recordlinkage
 import math as m
 
 def firstDFgenerator():
-    fb = pd.read_csv("./data/restaurants/gbr_splitted/FoodBuzz.csv", sep = ";")
-    fb_cols = ['restaurant', 'neighborhood', 'address', 'country', 'country_code', 'type', 'cost']
+    fb = pd.read_csv("./data/restaurants/gbr_splitted_google/FoodBuzz.csv", sep = ";")
+    fb_cols = ['restaurant', 'neighborhood', 'address', 'addressGoogle', 'country', 'country_code', 'type', 'cost']
     fb = fb[fb_cols]
 
-    nym = pd.read_csv("./data/restaurants/gbr_splitted/NYMag.csv", sep = ";")
+    nym = pd.read_csv("./data/restaurants/gbr_splitted_google/NYMag.csv", sep = ";")
     nym.rename(columns={"address": "neighborhood"}, inplace = True)
 
-    nyt = pd.read_csv("./data/restaurants/gbr_splitted/NYTimes.csv", sep = ";")
+    nyt = pd.read_csv("./data/restaurants/gbr_splitted_google/NYTimes.csv", sep = ";")
     
-    ot = pd.read_csv("./data/restaurants/gbr_splitted/OpenTable.csv", sep = ";")
+    ot = pd.read_csv("./data/restaurants/gbr_splitted_google/OpenTable.csv", sep = ";")
 
-    sc = pd.read_csv("./data/restaurants/gbr_splitted/SavoryCities.csv", sep = ";")
-    sc_cols = ['restaurant', 'neighborhood', 'address']
+    sc = pd.read_csv("./data/restaurants/gbr_splitted_google/SavoryCities.csv", sep = ";")
+    sc_cols = ['restaurant', 'neighborhood', 'address', 'addressGoogle']
     sc = sc[sc_cols]
 
 
@@ -40,7 +40,7 @@ def firstDFgenerator():
 
     compare_cl = recordlinkage.Compare()
     compare_cl.string('restaurant', 'restaurant', threshold=0.85, label='ristorante')
-    compare_cl.string('neighborhood', 'neighborhood', threshold=0.85, label='quartiere')
+    compare_cl.string('neighborhood', 'neighborhood', threshold=0.85, label='quartiere') 
 
     features = compare_cl.compute(candidate_links, df)
 
@@ -52,7 +52,7 @@ def firstDFgenerator():
     final_merge = account_merge.merge(df, left_on="level_1", right_index=True)
     final_merge.dropna(axis='columns', inplace=True, how = 'all') #rimuovo solo colonne tutte NaN
     final_merge.drop(columns = ['ristorante', 'quartiere', 'Score'], inplace=True) # so che sono 1 1 2
-
+    
     final_list = []
     all_indexes = []
     for i, row in final_merge.iterrows():
@@ -60,24 +60,26 @@ def firstDFgenerator():
         all_indexes.append(row.level_1)
 
         restaurant = row.restaurant_x
-        neighborhood = row.neighborhood_x
-        """
-        if row.neighborhood_x.lower() == row.neighborhood_y.lower():
-            neighborhood = row.neighborhood_x
+        neighborhood = row.neighborhood_x # da controllare
+
+        if row.addressGoogle_x == row.addressGoogle_y:
+            addressGoogle = str(row.addressGoogle_x)
+        elif (row.addressGoogle_x != row.addressGoogle_x): # è NaN
+            addressGoogle = str(row.addressGoogle_y) 
         else:
-            neighborhood = row.neighborhood_x + ";" + row.neighborhood_y
-        """
+            addressGoogle = str(row.addressGoogle_x)
+
 
         address = row.address_y
         country = row.country_y
         type_r = row.type_y
         cost = row.cost_y
         country_code = row.country_code_y
-        new_row = (restaurant, neighborhood, address, country, country_code, type_r, cost)
+        new_row = (restaurant, neighborhood, address, addressGoogle, country, country_code, type_r, cost)
         final_list.append(new_row)
 
     clean_merge = pd.DataFrame(final_list)
-    clean_merge.columns = ["restaurant", "neighborhood", "address", "country", "country_code", "type_r", "cost"]
+    clean_merge.columns = ["restaurant", "neighborhood", "address", "addressGoogle", "country", "country_code", "type_r", "cost"]
 
     df.drop(index = all_indexes, inplace = True, axis = 0)
 
@@ -89,47 +91,41 @@ def firstDFgenerator():
         new_index.append(i)
 
     df.index = new_index
-
-    df.to_csv("./data/restaurants_integrated/output_recordlinkage/prova_deduplication.csv", header=True, sep=";", decimal=',', float_format='%.3f', index=False)
+    
     return df
     
 def secondDFgenerator():
-    to = pd.read_csv("./data/restaurants/gbr_splitted/TimeOut.csv", sep = ";")
+    to = pd.read_csv("./data/restaurants/gbr_splitted_google/TimeOut.csv", sep = ";")
     temp_to = to['in'].map(str) + ' ' + to['address1'].map(str) + '-' + to['address2'].map(str)+ to['address3'].map(str)
     temp_to = temp_to.str.replace("nan", "")
     to.drop(columns = ['in', 'address1', 'address2', 'address3'], inplace = True, axis = 1)
     to['address'] = temp_to
     to['country'] = 'New York'
-    to_cols = ['restaurant', 'address', 'country', 'neighborhood', 'type', 'phone']
+    to_cols = ['restaurant', 'address', 'addressGoogle', 'country', 'neighborhood', 'type', 'phone']
     to = to[to_cols]
 
-    ad = pd.read_csv("./data/restaurants/gbr_splitted/ActiveDiner.csv", sep = ";")
+    ad = pd.read_csv("./data/restaurants/gbr_splitted_google/ActiveDiner.csv", sep = ";")
+    ad_cols = ['restaurant', 'address', 'addressGoogle', 'country']
+    ad = ad[ad_cols]
     
-    dg = pd.read_csv("./data/restaurants/gbr_splitted/DiningGuide.csv", sep = ";")
+    dg = pd.read_csv("./data/restaurants/gbr_splitted_google/DiningGuide.csv", sep = ";")
 
-    mp = pd.read_csv("./data/restaurants/gbr_splitted/MenuPages.csv", sep = ";")
+    mp = pd.read_csv("./data/restaurants/gbr_splitted_google/MenuPages.csv", sep = ";")
     temp_mp = mp['address1'].map(str) + '-' + mp['address2'].map(str)
     temp_mp = temp_mp.str.replace("nan", "")
     mp.drop(columns = ['address1', 'address2'], inplace = True, axis = 1)
     mp['address'] = temp_mp
-    mp_cols = ['restaurant', 'address']
+    mp.rename(columns={"addressGoogle1": "addressGoogle"}, inplace = True)
+    mp_cols = ['restaurant', 'address', 'addressGoogle']
     mp = mp[mp_cols]
 
-    """
-    NON HA INDIRIZZO
+    ts = pd.read_csv("./data/restaurants/gbr_splitted_google/TasteSpace.csv", sep = ";")
+    ts_cols = ['restaurant', 'address', 'addressGoogle', 'country']
+    ts = ts[ts_cols]
 
-    ny = pd.read_csv("data/restaurants/gbr_splitted/NewYork.csv", sep = ";")
-    ny.drop(columns = ['country_code'], inplace = True, axis = 1)
-    ny['address'] = 'NaN'
-    ny_cols = ['restaurant', 'address', 'country']
-    ny = ny[ny_cols]
-    """
-
-    ts = pd.read_csv("./data/restaurants/gbr_splitted/TasteSpace.csv", sep = ";")
-
-    vv = pd.read_csv("./data/restaurants/gbr_splitted/VillageVoice.csv", sep = ";")
+    vv = pd.read_csv("./data/restaurants/gbr_splitted_google/VillageVoice.csv", sep = ";")
     vv['country'] = 'New York'
-    vv_cols = ['restaurant', 'address', 'country', 'neighborhood']
+    vv_cols = ['restaurant', 'address', 'addressGoogle', 'country', 'neighborhood']
     vv = vv[vv_cols]
 
     df = to.append([ad,dg,mp,ts,vv])
@@ -143,8 +139,7 @@ def secondDFgenerator():
         new_index.append(i)
 
     df.index = new_index
-    df.to_csv("./data/restaurants_integrated/output_recordlinkage/temp_df.csv", header=True, sep=";", decimal=',', float_format='%.3f', index=False)
-     
+    
     # DEDUPLICATION
    
     indexer = recordlinkage.Index()
@@ -153,7 +148,8 @@ def secondDFgenerator():
     
     compare_cl = recordlinkage.Compare()
     compare_cl.string('restaurant', 'restaurant', threshold=0.85, label='ristorante')
-    compare_cl.string('address', 'address', threshold=0.65, label='indirizzo') 
+    #compare_cl.string('address', 'address', threshold=0.65, label='indirizzo') 
+    compare_cl.exact('addressGoogle', 'addressGoogle', label = 'indirizzo')
 
     features = compare_cl.compute(candidate_links, df)
     
@@ -165,8 +161,6 @@ def secondDFgenerator():
     final_merge = account_merge.merge(df, left_on="level_1", right_index=True)
     final_merge.dropna(axis='columns', inplace=True, how = 'all') #rimuovo solo colonne tutte NaN
     final_merge.drop(columns = ['ristorante', 'indirizzo','Score'], inplace=True) # so che sono 1 1 2
-    
-    final_merge.to_csv("./data/restaurants_integrated/output_recordlinkage/final_merge_df2.csv", header=True, sep=";", decimal=',', float_format='%.3f', index=False)
     
     final_list = []
     all_indexes = []
@@ -200,12 +194,19 @@ def secondDFgenerator():
             neighborhood = str(row.neighborhood_y) 
         else:
             neighborhood = str(row.neighborhood_x)
+        
+        if row.addressGoogle_x == row.addressGoogle_y:
+            addressGoogle = str(row.addressGoogle_x)
+        elif (row.addressGoogle_x != row.addressGoogle_x): # è NaN
+            addressGoogle = str(row.addressGoogle_y) 
+        else:
+            addressGoogle = str(row.addressGoogle_x)
 
-        new_row = (restaurant, address, country, neighborhood, type_r, phone)
+        new_row = (restaurant, address, addressGoogle, country, neighborhood, type_r, phone)
         final_list.append(new_row)
 
     clean_merge = pd.DataFrame(final_list)
-    clean_merge.columns = ["restaurant", "address", "country", "neighborhood", "type_r", "phone"]
+    clean_merge.columns = ["restaurant", "address", "addressGoogle", "country", "neighborhood", "type_r", "phone"]
     
     df.drop(index = all_indexes, inplace = True, axis = 0)
 
@@ -218,7 +219,6 @@ def secondDFgenerator():
 
     df.index = new_index
     
-    df.to_csv("./data/restaurants_integrated/output_recordlinkage/prova_deduplication_2.csv", header=True, sep=";", decimal=',', float_format='%.3f', index=False)
     return df
 
 """
@@ -226,7 +226,10 @@ def main():
 
     df1 = firstDFgenerator()
     df2 = secondDFgenerator()
-    print(df1, df2)
+    
+    df1.to_csv('./data/restaurants_integrated/output_recordlinkage/df1_ded_address.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
+    df2.to_csv('./data/restaurants_integrated/output_recordlinkage/df2_ded_addressGoogle.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
+	
 
 if __name__ == '__main__':
     main()
--- source/recordlinkage/record-linkage.py
+++ source/recordlinkage/record-linkage.py
@@ -19,7 +19,7 @@ def linkDB(df1, df2, type, classifier) :
 	elif type=="full":
 		indexer.full()
 	elif type=="block":
-		indexer.block(left_on="0_addressFix", right_on="1_addressFix") # per ora non abbiamo addressFix
+		indexer.block(left_on="0_addressGoogle", right_on="1_addressGoogle") 
 
 	candidate_links = indexer.index(df1,df2)
 	
@@ -29,34 +29,31 @@ def linkDB(df1, df2, type, classifier) :
 	comp.string('0_restaurant', '1_restaurant', threshold=0.85, label='ristorante')
 	comp.string('0_address', '1_address', threshold=0.85, label='indirizzoOriginale')
 	comp.string('0_neighborhood', '1_neighborhood', threshold=0.85, label='quartiere')
+	comp.exact('0_addressGoogle', '1_addressGoogle', label = 'indirizzoGoogle')
 	
 	features = comp.compute(candidate_links, df1, df2)
 
 	# 3 - CLASSIFICATION
 	# https://recordlinkage.readthedocs.io/en/latest/ref-classifiers.html#unsupervised
 
-	# ECM Classifier
-	ecm = recordlinkage.ECMClassifier()
-	ecm.fit_predict(features, match_index=None) # Train the classifier
-	e_matches = ecm.predict(features)
-	print(e_matches)
-	# prob_matches = ecm.prob(features)
-
-	# K MEANS Classifier
-	kmeans = recordlinkage.KMeansClassifier()
-	kmeans.fit_predict(features)
-	k_matches = kmeans.predict(features)
-	print(k_matches)
-
 	matches = []
 	
 	if classifier == "ecm":
+		ecm = recordlinkage.ECMClassifier()
+		ecm.fit_predict(features, match_index=None) # Train the classifier
+		e_matches = ecm.predict(features)
+		# print(e_matches)
+		# prob_matches = ecm.prob(features)
 		for i, j in e_matches:
 			record_1 = df1.loc[i]
 			record_2 = df2.loc[j]
 			record = tuple(record_1) + tuple(record_2)
 			matches.append(record)
 	elif classifier == "kmeans":
+		kmeans = recordlinkage.KMeansClassifier()
+		kmeans.fit_predict(features)
+		k_matches = kmeans.predict(features)
+		# print(k_matches)
 		for i, j in k_matches:
 			record_1 = df1.loc[i]
 			record_2 = df2.loc[j]
@@ -73,18 +70,16 @@ def main():
 	
 	df1 = merge_df.firstDFgenerator() 
 	df1 = df1.add_prefix('0_')	
-	# 0_restaurant 0_neighborhood 0_address 0_country 0_country_code 0_type 0_cost 0_type_r
 
 	df2 = merge_df.secondDFgenerator()
 	df2 = df2.add_prefix('1_')
-	 #1_restaurant 1_address 1_country 1_neighbourhood 1_phone 1_type_r
 	
 	ecm_result = linkDB(df1, df2, type="sortedneighbourhood", classifier = "ecm")
 	km_result = linkDB(df1, df2, type="sortedneighbourhood", classifier = "kmeans")
 
 	
-	ecm_result.to_csv('./data/restaurants_integrated/output_recordlinkage/final_matches_ecm.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
-	km_result.to_csv('./data/restaurants_integrated/output_recordlinkage/final_matches_km.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
+	ecm_result.to_csv('./data/restaurants_integrated/output_recordlinkage/final_matches_ecm_addressGoogle.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
+	km_result.to_csv('./data/restaurants_integrated/output_recordlinkage/final_matches_km_addressGoogle.csv', header=True, sep=";", decimal=',', float_format='%.3f', index=False)
 	
 	
 if __name__ == "__main__":
