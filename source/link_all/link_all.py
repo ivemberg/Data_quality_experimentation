9f696fdb681101e9a45ba82b41affc1c3bee18ea
Federico Alberici <f.alberici@campus.unimib.it>
2020-07-14 21:53:26 +0200

    link_all updates

--- source/link_all/link_all.py
+++ source/link_all/link_all.py
@@ -34,7 +34,7 @@ def dedupeDS(window: int, th: float) -> dict:
         print(f"{index_to_drop}\n")
         ds.drop(index_to_drop, inplace=True)
         datasets[rest] = ds.copy()
-    
+
     return datasets
 
 
@@ -56,7 +56,7 @@ def get_datasets(from_regx: str) -> dict:
     return datasets
 
 
-def link_reduce(from_rest: str, dfs: dict, window: int, th: float, classifier: str, thFusion:float) -> dict:
+def link_reduce(from_rest: str, dfs: dict, window: int, th: float, classifier: str, thFusion: float) -> dict:
     dfs_copy = {from_rest: dfs[from_rest]}
     dfs_reduce = dfs.copy()
 
@@ -78,7 +78,7 @@ def link_reduce(from_rest: str, dfs: dict, window: int, th: float, classifier: s
                     columns_to_check.append('addressGoogle')
                 if df['neighborhood'].isnull().sum() != len(df['neighborhood']) and ddf['neighborhood'].isnull().sum() != len(ddf['neighborhood']):
                     columns_to_check.append('neighborhood')
-                
+
                 #print(f"\tcheck: {columns_to_check}")
                 indexer = recordlinkage.Index()
 
@@ -94,31 +94,32 @@ def link_reduce(from_rest: str, dfs: dict, window: int, th: float, classifier: s
                     if col == 'addressGoogle':
                         compare_cl.exact(col, col)
                     else:
-                        compare_cl.string(col, col, label=col, threshold=th, method='jarowinkler')
+                        compare_cl.string(col, col, label=col,
+                                          threshold=th, method='jarowinkler')
                 features = compare_cl.compute(candidate_links, df, ddf)
 
                 # 3 - CLASSIFICATION
                 matches = None
-                matches = features[features.sum(axis=1) > len(columns_to_check)-1]
-                index_to_drop = set(matches.index.get_level_values(1))
-                # if classifier == "ecm":
-                #     ecm = recordlinkage.ECMClassifier(
-                #         init='jaro', binarize=None, max_iter=100, atol=0.0001, use_col_names=True)
-                #     ecm.fit_predict(features)
-                #     matches = ecm.predict(features)
-                # elif classifier == "kmeans":
-                #     kmeans = recordlinkage.KMeansClassifier()
-                #     kmeans.fit_predict(features)
-                #     matches = kmeans.predict(features)
-                
-                #print(f"\tmatches: {len(matches)}")
+                if classifier == "ecm":
+                    ecm = recordlinkage.ECMClassifier(
+                        init='jaro', binarize=None, max_iter=100, atol=0.0001, use_col_names=True)
+                    ecm.fit_predict(features)
+                    matches = ecm.predict(features)
+                elif classifier == "kmeans":
+                    kmeans = recordlinkage.KMeansClassifier()
+                    kmeans.fit_predict(features)
+                    matches = kmeans.predict(features)
+
                 # 4 - COMBINE INFORMATION
-                for left, right in matches.index:
-                    combine(df.loc[left], ddf.loc[right], thFusion)
+                for left, right in matches:
+                    if not combine(df.loc[left], ddf.loc[right], thFusion, th):
+                        matches = matches.drop((left, right))
+
+                print(f"\tmatches: {len(matches)}")
                 dfs_copy[rest] = df.copy()
 
                 # 4 - DROP RIGHT ON MATCHES INDEX
-                #index_to_drop = set(matches.get_level_values(1))
+                index_to_drop = set(matches.get_level_values(1))
                 #print(f"\tddf before drop: {len(ddf.index)}")
                 ddf.drop(index_to_drop, inplace=True)
                 dfs_copy[rr] = ddf.copy()
@@ -126,30 +127,53 @@ def link_reduce(from_rest: str, dfs: dict, window: int, th: float, classifier: s
                 #print(f"\tddf after drop: {len(dfs_reduce[rr].index)}\n")
 
         del dfs_reduce[rest]
-    
+
     final_df = pd.concat(list(dfs_copy.values()))
     final_df.dropna(subset=['addressGoogle'], inplace=True)
     final_df.drop_duplicates(inplace=True)
     return final_df
 
 
-def combine(left:pd.Series, right:pd.Series, thFusion:float):
-    # New information arrives from right Series, includes 'addressGoogle','neighborhood','type','cost','phone'   
+def combine(left: pd.Series, right: pd.Series, thFusion: float, th: float) -> bool:
+    # Another check on 'restaurant'
+    rest_dist = distance.get_jaro_distance(
+        left['restaurant'], right['restaurant'], winkler=True, scaling=0.1)
+    if rest_dist < th:
+        return False
+
+    # New information arrives from right Series, includes 'addressGoogle','neighborhood','type','cost','phone'
+    if right['type'] == 'nan':
+        right['type'] = None
+
+    if left['type'] == 'nan':
+        left['type'] = None
+
+    # Is a phone number
+    if right['type'] and sum(c.isdigit() for c in str(right['type'])) > 2 and right['phone']:
+        right['phone'] = right['type']
+        right['type'] = None
+
+    # Is a phone number
+    if left['type'] and sum(c.isdigit() for c in str(left['type'])) > 2 and left['phone']:
+        left['phone'] = left['type']
+        left['type'] = None
+
     if right['type']:
         if not left['type']:
             left['type'] = right['type']
         else:
             lft = str(left['type'])
             lefts = lft.split('/')
-            bInsert = False
+            bInsert = True
             for l in lefts:
                 rgt = str(right['type'])
-                dist = distance.get_jaro_distance(l, rgt, winkler=True, scaling=0.1)
-                if dist < thFusion:
-                    bInsert = True
+                dist = distance.get_jaro_distance(
+                    l, rgt, winkler=True, scaling=0.1)
+                if dist >= thFusion:
+                    bInsert = False
                     break
             if bInsert:
-                left['type'] = f"{left['type']} / {right['type']}" 
+                left['type'] = f"{left['type']} / {right['type']}"
 
     if right['phone']:
         if not left['phone']:
@@ -167,9 +191,12 @@ def combine(left:pd.Series, right:pd.Series, thFusion:float):
         if not left['neighborhood']:
             left['neighborhood'] = right['neighborhood']
 
+    return True
+
 
 def align_schema(dicIn: dict):
-    columns = ['restaurant','addressGoogle','neighborhood','country','country_code','type','cost','phone']
+    columns = ['restaurant', 'addressGoogle', 'neighborhood',
+               'country', 'country_code', 'type', 'cost', 'phone']
     for rest, df in dicIn.items():
         for column in columns:
             if not column in df.columns:
@@ -184,10 +211,23 @@ def align_schema(dicIn: dict):
 
 
 def main():
-    data = dedupeDS(9, 0.9)
+
+    # Settings
+    dedWindow = 9
+    dedTh = 0.9
+    linkStart = 'VillageVoice'
+    linkMl = 'ecm'
+    linkWindow = 9
+    linkTh = 0.9
+    linkThType = 0.8
+    # End settings
+
+    data = dedupeDS(dedWindow, dedTh)
     align_schema(data)
-    reduced = link_reduce('VillageVoice', data, 9, 0.9, "ecm", 0.8)
-    reduced.to_csv('test.csv', sep=';', index=None)
+    reduced = link_reduce(linkStart, data, linkWindow,
+                          linkTh, linkMl, linkThType)
+    reduced.to_csv(
+        f"./data/restaurants_integrated/output_link_all/linkall_{dedWindow}_{dedTh}_{linkStart}_{linkMl}_{linkWindow}_{linkTh}_{linkThType}.csv", sep=';', index=None)
 
 
 if __name__ == '__main__':
